d6d946217d150d0dddad1ca9d9361ae95fea1967
ranaalisaeed <ali.saeedrana@gmail.com>
2021-07-23 14:32:56 +1000

    Initial local setup

--- components/homepage/HeroSection.js
+++ components/homepage/HeroSection.js
@@ -38,10 +38,10 @@ export default function HeroSection() {
               }}
             >
               <p className="font-size-display5 font-family-secondary mb-4 text-center hero-header">
-                The care you've always needed
+                Stylish dresses for you
               </p>
               <p className="text-transform-uppercase font-size-title mb-5 hero-subheader">
-                A range of products for you
+                A range of products
               </p>
               <Link href="/collection">
                 <a className="d-flex align-items-center bg-transparent border border-color-white h-56 px-5 font-color-white hero-btn">
